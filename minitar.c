a8d6546a3de963c5996fd37856a05f5ec4ceb973
petermckinley <petermckinley442@gmail.com>
2025-09-18 22:20:27 +0000

    added update funciton to bottom, needs to be filled

--- minitar.c
+++ minitar.c
@@ -30,14 +30,14 @@
  * standard for tar file structure.
  */
 void compute_checksum(tar_header *header) {
-    // Have to initially set header's checksum to "all blanks"
-    memset(header->chksum, ' ', 8);
-    unsigned sum = 0;
-    char *bytes = (char *) header;
-    for (int i = 0; i < sizeof(tar_header); i++) {
-        sum += bytes[i];
-    }
-    snprintf(header->chksum, 8, "%07o", sum);
+  // Have to initially set header's checksum to "all blanks"
+  memset(header->chksum, ' ', 8);
+  unsigned sum = 0;
+  char *bytes = (char *)header;
+  for (int i = 0; i < sizeof(tar_header); i++) {
+    sum += bytes[i];
+  }
+  snprintf(header->chksum, 8, "%07o", sum);
 }
 
 /*
@@ -46,57 +46,61 @@ void compute_checksum(tar_header *header) {
  * Returns 0 on success or -1 if an error occurs
  */
 int fill_tar_header(tar_header *header, const char *file_name) {
-    memset(header, 0, sizeof(tar_header));
-    char err_msg[MAX_MSG_LEN];
-    struct stat stat_buf;
-    // stat is a system call to inspect file metadata
-    if (stat(file_name, &stat_buf) != 0) {
-        snprintf(err_msg, MAX_MSG_LEN, "Failed to stat file %s", file_name);
-        perror(err_msg);
-        return -1;
-    }
+  memset(header, 0, sizeof(tar_header));
+  char err_msg[MAX_MSG_LEN];
+  struct stat stat_buf;
+  // stat is a system call to inspect file metadata
+  if (stat(file_name, &stat_buf) != 0) {
+    snprintf(err_msg, MAX_MSG_LEN, "Failed to stat file %s", file_name);
+    perror(err_msg);
+    return -1;
+  }
 
-    strncpy(header->name, file_name,
-            100);    // Name of the file, null-terminated string
-    snprintf(header->mode, 8, "%07o",
-             stat_buf.st_mode & 07777);    // Permissions for file, 0-padded octal
+  strncpy(header->name, file_name,
+          100); // Name of the file, null-terminated string
+  snprintf(header->mode, 8, "%07o",
+           stat_buf.st_mode & 07777); // Permissions for file, 0-padded octal
 
-    snprintf(header->uid, 8, "%07o",
-             stat_buf.st_uid);                         // Owner ID of the file, 0-padded octal
-    struct passwd *pwd = getpwuid(stat_buf.st_uid);    // Look up name corresponding to owner ID
-    if (pwd == NULL) {
-        snprintf(err_msg, MAX_MSG_LEN, "Failed to look up owner name of file %s", file_name);
-        perror(err_msg);
-        return -1;
-    }
-    strncpy(header->uname, pwd->pw_name,
-            32);    // Owner name of the file, null-terminated string
+  snprintf(header->uid, 8, "%07o",
+           stat_buf.st_uid); // Owner ID of the file, 0-padded octal
+  struct passwd *pwd =
+      getpwuid(stat_buf.st_uid); // Look up name corresponding to owner ID
+  if (pwd == NULL) {
+    snprintf(err_msg, MAX_MSG_LEN, "Failed to look up owner name of file %s",
+             file_name);
+    perror(err_msg);
+    return -1;
+  }
+  strncpy(header->uname, pwd->pw_name,
+          32); // Owner name of the file, null-terminated string
 
-    snprintf(header->gid, 8, "%07o",
-             stat_buf.st_gid);                        // Group ID of the file, 0-padded octal
-    struct group *grp = getgrgid(stat_buf.st_gid);    // Look up name corresponding to group ID
-    if (grp == NULL) {
-        snprintf(err_msg, MAX_MSG_LEN, "Failed to look up group name of file %s", file_name);
-        perror(err_msg);
-        return -1;
-    }
-    strncpy(header->gname, grp->gr_name,
-            32);    // Group name of the file, null-terminated string
+  snprintf(header->gid, 8, "%07o",
+           stat_buf.st_gid); // Group ID of the file, 0-padded octal
+  struct group *grp =
+      getgrgid(stat_buf.st_gid); // Look up name corresponding to group ID
+  if (grp == NULL) {
+    snprintf(err_msg, MAX_MSG_LEN, "Failed to look up group name of file %s",
+             file_name);
+    perror(err_msg);
+    return -1;
+  }
+  strncpy(header->gname, grp->gr_name,
+          32); // Group name of the file, null-terminated string
 
-    snprintf(header->size, 12, "%011o",
-             (unsigned) stat_buf.st_size);    // File size, 0-padded octal
-    snprintf(header->mtime, 12, "%011o",
-             (unsigned) stat_buf.st_mtime);    // Modification time, 0-padded octal
-    header->typeflag = REGTYPE;                // File type, always regular file in this project
-    strncpy(header->magic, MAGIC, 6);          // Special, standardized sequence of bytes
-    memcpy(header->version, "00", 2);          // A bit weird, sidesteps null termination
-    snprintf(header->devmajor, 8, "%07o",
-             major(stat_buf.st_dev));    // Major device number, 0-padded octal
-    snprintf(header->devminor, 8, "%07o",
-             minor(stat_buf.st_dev));    // Minor device number, 0-padded octal
+  snprintf(header->size, 12, "%011o",
+           (unsigned)stat_buf.st_size); // File size, 0-padded octal
+  snprintf(header->mtime, 12, "%011o",
+           (unsigned)stat_buf.st_mtime); // Modification time, 0-padded octal
+  header->typeflag = REGTYPE; // File type, always regular file in this project
+  strncpy(header->magic, MAGIC, 6); // Special, standardized sequence of bytes
+  memcpy(header->version, "00", 2); // A bit weird, sidesteps null termination
+  snprintf(header->devmajor, 8, "%07o",
+           major(stat_buf.st_dev)); // Major device number, 0-padded octal
+  snprintf(header->devminor, 8, "%07o",
+           minor(stat_buf.st_dev)); // Minor device number, 0-padded octal
 
-    compute_checksum(header);
-    return 0;
+  compute_checksum(header);
+  return 0;
 }
 
 /*
@@ -106,32 +110,31 @@ int fill_tar_header(tar_header *header, const char *file_name) {
  * learn about later
  */
 int remove_trailing_bytes(const char *file_name, size_t nbytes) {
-    char err_msg[MAX_MSG_LEN];
+  char err_msg[MAX_MSG_LEN];
 
-    struct stat stat_buf;
-    if (stat(file_name, &stat_buf) != 0) {
-        snprintf(err_msg, MAX_MSG_LEN, "Failed to stat file %s", file_name);
-        perror(err_msg);
-        return -1;
-    }
+  struct stat stat_buf;
+  if (stat(file_name, &stat_buf) != 0) {
+    snprintf(err_msg, MAX_MSG_LEN, "Failed to stat file %s", file_name);
+    perror(err_msg);
+    return -1;
+  }
 
-    off_t file_size = stat_buf.st_size;
-    if (nbytes > file_size) {
-        file_size = 0;
-    } else {
-        file_size -= nbytes;
-    }
+  off_t file_size = stat_buf.st_size;
+  if (nbytes > file_size) {
+    file_size = 0;
+  } else {
+    file_size -= nbytes;
+  }
 
-    if (truncate(file_name, file_size) != 0) {
-        snprintf(err_msg, MAX_MSG_LEN, "Failed to truncate file %s", file_name);
-        perror(err_msg);
-        return -1;
-    }
-    return 0;
+  if (truncate(file_name, file_size) != 0) {
+    snprintf(err_msg, MAX_MSG_LEN, "Failed to truncate file %s", file_name);
+    perror(err_msg);
+    return -1;
+  }
+  return 0;
 }
 
 int create_archive(const char *archive_name, const file_list_t *files) {
-<<<<<<< HEAD
   int fd = open(archive_name, O_WRONLY | O_CREAT | O_TRUNC, 0666);
   char BUFFER[BLOCK_SIZE];
 
@@ -178,7 +181,7 @@ int append_files_to_archive(const char *archive_name,
     return -1;
   }
 
-  lseek(fd, BLOCK_SIZE * 2 * -1, SEEK_END);
+  remove_trailing_bytes(archive_name, 2 * BLOCK_SIZE);
 
   node_t *current = files->head;
   while (current != NULL) {
@@ -197,86 +200,12 @@ int append_files_to_archive(const char *archive_name,
       total_bytes += bytes_read;
     }
 
-    size_t pad = (BLOCK_SIZE - (total_bytes % BLOCK_SIZE));
+    size_t pad = (BLOCK_SIZE - (total_bytes % BLOCK_SIZE)) % BLOCK_SIZE;
     if (pad > 0) {
       memset(BUFFER, 0, BLOCK_SIZE);
       write(fd, BUFFER, pad);
-=======
-    int fd = open(archive_name, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-    char BUFFER[BLOCK_SIZE];
-
-    node_t *current = files->head;
-    while (current != NULL) {
-        const char *file_name = current->name;
-        tar_header header;
-
-        int input_fd = open(file_name, O_RDONLY);
-
-        fill_tar_header(&header, file_name);
-        write(fd, &header, BLOCK_SIZE);
-        ssize_t bytes_read;
-        size_t total_bytes = 0;
-
-        while ((bytes_read = read(input_fd, BUFFER, BLOCK_SIZE)) > 0) {
-            write(fd, BUFFER, bytes_read);
-            total_bytes += bytes_read;
-        }
-
-        size_t pad = (BLOCK_SIZE - (total_bytes % BLOCK_SIZE)) % BLOCK_SIZE;
-        if (pad > 0) {
-            memset(BUFFER, 0, BLOCK_SIZE);
-            write(fd, BUFFER, pad);
-        }
-
-        current = current->next;
     }
 
-    memset(BUFFER, 0, BLOCK_SIZE);
-    write(fd, BUFFER, BLOCK_SIZE);
-    write(fd, BUFFER, BLOCK_SIZE);
-
-    close(fd);
-    return 0;
-}
-
-int append_files_to_archive(const char *archive_name, const file_list_t *files) {
-    int fd = open(archive_name, O_WRONLY | O_APPEND, 0666);
-    char BUFFER[BLOCK_SIZE];
-    if (!fd) {
-        close(fd);
-        return -1;
-    }
-
-    remove_trailing_bytes(archive_name, 2 * BLOCK_SIZE);
-
-    node_t *current = files->head;
-    while (current != NULL) {
-        const char *file_name = current->name;
-        tar_header header;
-
-        int input_fd = open(file_name, O_RDONLY);
-
-        fill_tar_header(&header, file_name);
-        write(fd, &header, BLOCK_SIZE);
-        ssize_t bytes_read;
-        size_t total_bytes = 0;
-
-        while ((bytes_read = read(input_fd, BUFFER, BLOCK_SIZE)) > 0) {
-            write(fd, BUFFER, bytes_read);
-            total_bytes += bytes_read;
-        }
-
-        size_t pad = (BLOCK_SIZE - (total_bytes % BLOCK_SIZE)) % BLOCK_SIZE;
-        if (pad > 0) {
-            memset(BUFFER, 0, BLOCK_SIZE);
-            write(fd, BUFFER, pad);
-        }
-
-        current = current->next;
->>>>>>> 7b0b4ff0b6becf6e3e409329bb517b2f158d23af
-    }
-
-<<<<<<< HEAD
     current = current->next;
   }
   memset(BUFFER, 0, BLOCK_SIZE);
@@ -311,24 +240,4 @@ int get_archive_file_list(const char *archive_name, file_list_t *files) {
 int extract_files_from_archive(const char *archive_name) {
   // TODO: Not yet implemented
   return 0;
-=======
-    close(fd);
-    return 0;
-}
-
-int get_archive_file_list(const char *archive_name, file_list_t *files) {
-    return 0;
-}
-
-int extract_files_from_archive(const char *archive_name) {
-    int fp = open(archive_name, O_WRONLY | O_APPEND, 0666);
-    // char BUFFER[BLOCK_SIZE];
-    if (!fp) {
-        close(fp);
-        return -1;
-    }
-
-    close(fp);
-    return 0;
->>>>>>> 7b0b4ff0b6becf6e3e409329bb517b2f158d23af
 }
--- minitar.h
+++ minitar.h
@@ -4,39 +4,39 @@
 
 // Standard tar header layout defined by POSIX
 typedef struct {
-    // File's name, as a null-terminated string
-    char name[100];
-    // File's permission bits
-    char mode[8];
-    // Numerical ID of file's owner, 0-padded octal
-    char uid[8];
-    // Numerical ID of file's group, 0-padded octal
-    char gid[8];
-    // Size of file in bytes, 0-padded octal
-    char size[12];
-    // Modification time of file in Unix epoch time, 0-padded octal
-    char mtime[12];
-    // Checksum (simple sum) header bytes, 0-padded octal
-    char chksum[8];
-    // File type (use constants defined below)
-    char typeflag;
-    // Unused for this project
-    char linkname[100];
-    // Indicates which tar standard we are using
-    char magic[6];
-    char version[2];
-    // Name of file's user, as a null-terminated string
-    char uname[32];
-    // Name of file's group, as a null-terminated string
-    char gname[32];
-    // Major device number, 0-padded octal
-    char devmajor[8];
-    // Minor device number, 0-padded octal
-    char devminor[8];
-    // String to prepend to file name above, if name is longer than 100 bytes
-    char prefix[155];
-    // Padding to bring total struct size up to 512 bytes
-    char padding[12];
+  // File's name, as a null-terminated string
+  char name[100];
+  // File's permission bits
+  char mode[8];
+  // Numerical ID of file's owner, 0-padded octal
+  char uid[8];
+  // Numerical ID of file's group, 0-padded octal
+  char gid[8];
+  // Size of file in bytes, 0-padded octal
+  char size[12];
+  // Modification time of file in Unix epoch time, 0-padded octal
+  char mtime[12];
+  // Checksum (simple sum) header bytes, 0-padded octal
+  char chksum[8];
+  // File type (use constants defined below)
+  char typeflag;
+  // Unused for this project
+  char linkname[100];
+  // Indicates which tar standard we are using
+  char magic[6];
+  char version[2];
+  // Name of file's user, as a null-terminated string
+  char uname[32];
+  // Name of file's group, as a null-terminated string
+  char gname[32];
+  // Major device number, 0-padded octal
+  char devmajor[8];
+  // Minor device number, 0-padded octal
+  char devminor[8];
+  // String to prepend to file name above, if name is longer than 100 bytes
+  char prefix[155];
+  // Padding to bring total struct size up to 512 bytes
+  char padding[12];
 } tar_header;
 
 /*
@@ -51,19 +51,19 @@ typedef struct {
 int create_archive(const char *archive_name, const file_list_t *files);
 
 /*
- * Append each file specified in 'files' to the archive with the name 'archive_name'.
- * You can assume in this project that at least one new file to append is specified.
- * You may also assume that all files to be appended exist.
+ * Append each file specified in 'files' to the archive with the name
+ * 'archive_name'. You can assume in this project that at least one new file to
+ * append is specified. You may also assume that all files to be appended exist.
  * This function should return 0 upon success or -1 if an error occurred.
  */
 int append_files_to_archive(const char *archive_name, const file_list_t *files);
 
 /*
- * Add the name of each file contained in the archive identified by 'archive_name'
- * to the 'files' list.
- * NOTE: This function is most obviously relevant to implementing minitar's list
- * operation, but think about how you can reuse it for the update operation.
- * This function should return 0 upon success or -1 if an error occurred.
+ * Add the name of each file contained in the archive identified by
+ * 'archive_name' to the 'files' list. NOTE: This function is most obviously
+ * relevant to implementing minitar's list operation, but think about how you
+ * can reuse it for the update operation. This function should return 0 upon
+ * success or -1 if an error occurred.
  */
 int get_archive_file_list(const char *archive_name, file_list_t *files);
 
@@ -77,4 +77,6 @@ int get_archive_file_list(const char *archive_name, file_list_t *files);
  */
 int extract_files_from_archive(const char *archive_name);
 
-#endif    // _MINITAR_H
+int update_files_in_archive(const char *archive_name, const file_list_t *files);
+
+#endif // _MINITAR_H
